bb092cf874e3caf2e13d122c72344ea99779085d
Evan Lucas <evanlucas@me.com>
2015-04-20 12:53:39 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,46 @@
+# fetch-patch
+
+fetch a patch from github (works for private repos too)
+
+Useful for piping into `git am`
+
+## Install
+
+```bash
+$ npm install -g fetch-patch
+```
+
+## Usage
+
+```
+$ fetch-patch help
+
+fetch-patch
+
+  fetch a patch from github (works for private repos too)
+
+  usage:
+    fetch-patch <patch url>
+    fetch-patch <repo owner> <repo name> <pr url>
+
+  note:
+    in order for private repositories to work correctly, one must create a
+    github token and place it in ~/.github_token
+```
+
+## Example
+
+```bash
+$ fetch-patch https://github.com/iojs/io.js/pull/1457.patch | git am
+
+# or
+$ fetch-patch iojs io.js 1457 | git am
+```
+
+## Author
+
+Evan Lucas
+
+## License
+
+MIT (See `LICENSE` for more info)
---
+++ bin/cmd.js
@@ -0,0 +1,62 @@
+#!/usr/bin/env node
+
+var args = process.argv.splice(2)
+  , patch = require('../')
+  , fs = require('fs')
+  , home = process.env.HOME
+  , path = require('path')
+
+if (!args.length) return usage(1)
+
+if (args[0] === 'help' || args[0] === '-h' || args[0] === '--help') {
+  return usage(0)
+}
+
+var token = readToken()
+
+if (args.length === 1) {
+  var url
+  if (token) {
+    url = `${args[0]}?access_token=${token}`
+  } else {
+    url = args[0]
+  }
+  patch.fetchPatch(url, done)
+} else if (args.length === 3) {
+  var opts = {
+    owner: args[0]
+  , repo: args[1]
+  , pr: args[2]
+  , token: token
+  }
+
+  patch.fetchPatchWithOpts(opts, done)
+
+} else {
+  usage(1)
+}
+
+function done(err, out) {
+  if (err) {
+    console.error('ERR:', err)
+    process.exit(1)
+  }
+
+  console.log(out)
+}
+
+function readToken() {
+  var token
+  try {
+    return fs.readFileSync(path.join(home, '.github_token'), 'utf8').trim()
+  }
+  catch (err) {}
+}
+
+function usage(code) {
+  var rs = fs.createReadStream(__dirname + '/usage.txt')
+  rs.pipe(process.stdout)
+  rs.on('close', function() {
+    if (code) process.exit(code)
+  })
+}
